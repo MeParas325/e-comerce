47c704aaa60228c7001a35e4e5140293dc5ce8a7
MeParas325 <parasverma2857@gmail.com>
2026-01-23 09:35:06 +0530

    Added: DB config & other tools(Cloudinary, Inngest)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import path from 'path';
 import {ENV} from './config/env.js';
+import {connectDB} from './config/db.js';
+import {clerkMiddleware} from '@clerk/express';
 
 const app = express();
 
 const __dirname = path.resolve();
+
+// adds auth object in req => req.auth
+app.use(clerkMiddleware());
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({message: 'Success'});
@@ -21,4 +26,5 @@ if(ENV.NODE_ENV == 'production') {
 
 app.listen(3000, () => {
     console.log('Server is up and running');
+    connectDB();
 })
